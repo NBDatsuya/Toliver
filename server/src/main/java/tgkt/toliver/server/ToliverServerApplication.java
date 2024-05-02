3a611ba6e98e374622b068b0fb01c54351390c82
NBDatsuya <2646809050@qq.com>
2024-05-02 18:28:32 +0800

    init commit

---
+++ server/src/main/java/tgkt/toliver/server/ToliverServerApplication.java
@@ -0,0 +1,13 @@
+package tgkt.toliver.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToliverServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToliverServerApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/tgkt/toliver/server/ToliverServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tgkt.toliver.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToliverServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
